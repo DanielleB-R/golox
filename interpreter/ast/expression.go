c079047752d909f1047d9490211e255ad38a4d2d
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2022-03-15 10:54:47 -0700

    Implement an AST with visitor pattern

---
+++ interpreter/ast/expression.go
@@ -0,0 +1,61 @@
+package ast
+
+import "github.com/DanielleB-R/golox/interpreter/token"
+
+var (
+	_ Expr = (*Binary)(nil)
+	_ Expr = (*Grouping)(nil)
+	_ Expr = (*Literal)(nil)
+	_ Expr = (*Unary)(nil)
+)
+
+type Expr interface {
+	expression()
+	Accept(visitor Visitor) interface{}
+}
+
+type Visitor interface {
+	VisitBinary(binary *Binary) interface{}
+	VisitGrouping(grouping *Grouping) interface{}
+	VisitLiteral(literal *Literal) interface{}
+	VisitUnary(unary *Unary) interface{}
+}
+
+type Binary struct {
+	Left     Expr
+	Operator token.Token
+	Right    Expr
+}
+
+func (*Binary) expression() {}
+func (b *Binary) Accept(visitor Visitor) interface{} {
+	return visitor.VisitBinary(b)
+}
+
+type Grouping struct {
+	Expression Expr
+}
+
+func (*Grouping) expression() {}
+func (g *Grouping) Accept(visitor Visitor) interface{} {
+	return visitor.VisitGrouping(g)
+}
+
+type Literal struct {
+	Value interface{}
+}
+
+func (*Literal) expression() {}
+func (l *Literal) Accept(visitor Visitor) interface{} {
+	return visitor.VisitLiteral(g)
+}
+
+type Unary struct {
+	Operator token.Token
+	Right    Expr
+}
+
+func (*Unary) expression() {}
+func (u *Unary) Accept(visitor Visitor) interface{} {
+	return visitor.VisitUnary(u)
+}
---
+++ interpreter/ast/printer.go
@@ -0,0 +1,40 @@
+package ast
+
+import (
+	"fmt"
+	"strings"
+)
+
+type AstPrinter struct{}
+
+func (p *AstPrinter) Print(expr Expr) string {
+	return expr.Accept(p).(string)
+}
+
+func (p *AstPrinter) VisitBinary(binary *Binary) interface{} {
+	return p.parenthesize(binary.Operator.Lexeme, binary.Left, binary.Right)
+}
+
+func (p *AstPrinter) VisitGrouping(grouping *Grouping) interface{} {
+	return p.parenthesize("group", grouping.Expression)
+}
+
+func (p *AstPrinter) VisitLiteral(literal *Literal) interface{} {
+	if literal.Value == nil {
+		return "nil"
+	}
+	return fmt.Sprint(literal.Value)
+}
+
+func (p *AstPrinter) VisitUnary(unary *Unary) interface{} {
+	return p.parenthesize(unary.Operator.Lexeme, unary.Right)
+}
+
+func (p *AstPrinter) parenthesize(name string, exprs ...Expr) string {
+	exprRepresentations := []string{}
+	for _, expr := range exprs {
+		exprRepresentations = append(exprRepresentations, expr.Accept(p).(string))
+	}
+
+	return fmt.Sprintf("(%s %s)", name, strings.Join(exprRepresentations, " "))
+}
